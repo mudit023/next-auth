ecd2bfcfb75de664ae390c4d3442fcf00158a0f2
Mudit Mishra <Mudit.12016298@lpu.in>
2023-07-15 00:38:29 +0530

    added profile and other style changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,24 +3,21 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import Link from "next/link";
+
 export default function ProfilePage() {
   const router = useRouter();
-  const [userDetail, setUserDetail] = useState({
-    username: "",
-    email: "",
-    created_at: "",
-    isAdmin: "",
-    isVerified: "",
-  });
+  const [userDetail, setUserDetail] = useState("");
   const [loader, setLoader] = useState(false);
   useEffect(() => {
-    setLoader(true);
     async function getUserDetail() {
       try {
+        setLoader(true);
         const res = await axios.get("/api/user/user_detail");
         console.log("user data:", res.data.data);
         if (res?.data?.data) {
-          setUserDetail(res?.data?.data);
+          localStorage.setItem("userDetail", res?.data?.data?.username);
+          setUserDetail(res?.data?.data?.username);
         } else {
           toast.error("Error occurred");
         }
@@ -30,17 +27,23 @@ export default function ProfilePage() {
         else toast.error(String(error));
       }
     }
-    getUserDetail();
+    if (localStorage.getItem("userDetail")) {
+      setLoader(true);
+      const data = localStorage.getItem("userDetail")!;
+      setUserDetail(data);
+      setLoader(false);
+    } else {
+      getUserDetail();
+    }
   }, []);
   function logoutHandler() {
     const loadingToast = toast.loading("Logging Out...");
+    localStorage.removeItem("userDetail");
     try {
       const res = axios.get("api/user/logout");
       console.log("logout successful");
       toast.success("Logout Successfully!", { id: loadingToast });
-      setTimeout(() => {
-        router.push("/login");
-      }, 500);
+      router.push("/login");
     } catch (error: any) {
       if (error.response) {
         toast.error(error.response.data.error, {
@@ -62,17 +65,31 @@ export default function ProfilePage() {
     }
   }
   return (
-    <div>
+    <>
       <div>
         <Toaster />
       </div>
-      <h1>{`Howdy👋 ${userDetail?.username.toLocaleUpperCase()}`}</h1>
-      <button
-        onClick={logoutHandler}
-        className="border border-solid border-yellow-400 p-2"
-      >
-        Logout
-      </button>
-    </div>
+      <div className="p-2 flex-col justify-center items-center gap-4">
+        {loader ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <button
+              onClick={logoutHandler}
+              className="fixed top-2 right-2 border border-solid border-cyan-700 shadow-md shadow-cyan-800 px-2 py-1 text-sm rounded-sm"
+            >
+              logout
+            </button>
+            <h1 className="text-2xl font-bold tracking-wide">{`Howdy👋 ${userDetail.toUpperCase()}`}</h1>
+            <Link
+              href={`/profile/${userDetail}`}
+              className="px-2 py-1 bg-cyan-300 text-slate-950 inline-block mt-2"
+            >
+              User Profile
+            </Link>
+          </>
+        )}
+      </div>
+    </>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPathPublic = path==='/login' || path==='/signup'
+  const isPathPublic = path==='/login' || path==='/signup' || path==="/"
   const token = request.cookies.get("token")?.value || "";
   if(isPathPublic && token){
     return NextResponse.redirect(new URL("/profile", request.nextUrl))
