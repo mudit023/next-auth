1edc84435f98c6218921bb0963c45a8fbbe34c90
Mudit Mishra <Mudit.12016298@lpu.in>
2023-07-17 16:57:58 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-2">
+    <main className="flex flex-col items-center justify-center gap-5 p-2">
       <h1 className="sm:text-8xl text-4xl font-extrabold tracking-wide text-white">
         NEXT <span className="text-shadow-lg shadow-cyan-800">Auth</span>
       </h1>
@@ -12,6 +12,17 @@ export default function Home() {
       >
         Try Next Auth
       </Link>
+
+      <footer className="fixed bottom-0 text-sm mb-1">
+        Made by{" "}
+        <a
+          href="https://twitter.com/muditwt"
+          target="_blank"
+          className="underline"
+        >
+          Mudit
+        </a>
+      </footer>
     </main>
   );
 }
