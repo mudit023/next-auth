ba335e8dda6ac4dfa876fa8ebfbcb5636289c9fa
Mudit Mishra <Mudit.12016298@lpu.in>
2023-07-17 16:28:35 +0530

    fix home page responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-4">
-      <h1 className="text-8xl font-extrabold tracking-wide text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-2">
+      <h1 className="sm:text-8xl text-4xl font-extrabold tracking-wide text-white">
         NEXT <span className="text-shadow-lg shadow-cyan-800">Auth</span>
       </h1>
       <Link
